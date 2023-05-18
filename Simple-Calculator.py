8eafb259787edcbf9cb6b70ce8f5d357269bb775
Gurpreet singh <113198284+Sharper34@users.noreply.github.com>
2023-05-18 12:25:42 -0400

    Add files via upload

---
+++ Simple-Calculator.py
@@ -0,0 +1,44 @@
+print("Name: Gurpreet Singh")
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y == 0:
+        return "Error! Division by zero is not allowed."
+    else:
+        return x / y
+
+while True:
+    print("Select operation:")
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
+    print("5. Exit calculator")
+
+    choice = input("Enter choice(1/2/3/4/5): ")
+
+    if choice == '5':
+        print("Exiting the calculator. Good Bye!")
+        break
+
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+
+    if choice == '1':
+        print(f"The result is: {add(num1, num2)}")
+    elif choice == '2':
+        print(f"The result is: {subtract(num1, num2)}")
+    elif choice == '3':
+        print(f"The result is: {multiply(num1, num2)}")
+    elif choice == '4':
+        print(f"The result is: {divide(num1, num2)}")
+    else:
+        print("Invalid Input")
