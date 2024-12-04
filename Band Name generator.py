36ba73fa011237678584789137eba7dfc8412849
Gurpreet singh <113198284+Sharper34@users.noreply.github.com>
2024-12-04 15:01:20 -0500

    Add files via upload

---
+++ Band Name generator.py
@@ -0,0 +1,7 @@
+#This is a simple Band name Generator!
+print("Welcome to Band Name Generator!")
+Place = input("What is the name of the city you were born?")
+print(Place)
+Dream = input('whats your favorate Color?')
+print(Dream)
+print(Place + " " + Dream)
---
+++ Text based Riddle game.py
@@ -0,0 +1,27 @@
+#print A welcome Message
+print("welcome to the Riddler Adventure")
+print("Hey! I am Riddler")
+print("I will be asking you some Fun and Easy Riddles Question")
+
+User = input("Are you ready:")
+if (User == "Yes"):
+    print("Then lets get Start")
+elif (User == "No"):
+    quit()
+
+print("Pick the Difficulty")
+print("Easy" )
+print("Medium")
+print("Hard")
+User = input("")
+if (User == "Easy" , "easy"):
+    print("<<<Begin>>>")   
+elif (User == "Medium" , "medium"):
+    print("<<<Begin>>>")
+elif (User == "Hard" , "hard"):
+    print("<<<Begin>>>")
+else:
+    quit()
+#print("ROUND ONE")
+
+    
